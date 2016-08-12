3e728dba67898f325a224c7fd22271f475566c05
Siddhant Bansal <bansal.siddhant@gmail.com>
2016-08-12 09:37:21 +0530

    Finished functionality

---
+++ Assets/ClickHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickHandler : MonoBehaviour {
+
+    public GameObject ball;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        	
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("Click.");
+    }
+}
---
+++ Assets/EndScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndScript : MonoBehaviour {
+
+    public GameObject ball;
+    public Text endText;
+    Rigidbody2D rb2d;
+    public BallMover ballMoverScript;
+
+    void Start ()
+    {
+        rb2d = ball.GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D()
+    {
+        endText.gameObject.SetActive(true);
+        rb2d.velocity = Vector2.zero;
+        ballMoverScript.gameObject.SetActive(false);
+    }
+}
