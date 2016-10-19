7e4c7c08c4fe795005deea8510cc8f26ed5aff58
Siddhant Bansal <bansal.siddhant@gmail.com>
2016-10-19 22:34:01 +0530

    Pause game/record distance

--- Assets/Scripts/ClickHandler.cs
+++ Assets/Scripts/ClickHandler.cs
@@ -3,18 +3,26 @@ using System.Collections;
 
 public class ClickHandler : MonoBehaviour {
 
-    public GameObject ball;
+    GameObject ball;
+    Vector2 ballPos;
+    BallMover mover;
+    SpriteRenderer sr;
 
-	void Start () {
-	
-	}
-	
-	void Update () {
-        	
-	}
+    void Start()
+    {
+        ball = GameObject.Find("Ball");
+        mover = ball.GetComponent<BallMover>();
+        sr = ball.GetComponent<SpriteRenderer>();
+    }
 
     void OnMouseDown()
     {
-        Debug.Log("Click.");
+        ballPos = new Vector2(ball.transform.position.x, ball.transform.position.y);
+        Vector2 pixelPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(pixelPos);
+        Vector2 distance = ballPos - mousePos;
+        Debug.Log("dist: " + distance.magnitude);
+        mover.speed = 0;
+        sr.sortingLayerName = "ClosedPipe";
     }
 }
